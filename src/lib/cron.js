02ab11902a18a59a8d1dedc1b72df534d6ab800d
AlphaPenguino <zunder.pacis@lpunetwork.edu.ph>
2025-07-07 22:27:10 +0800

    cronjab

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -2,13 +2,15 @@ import cron from "cron";
 import https from "https";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
-    https.get(process.env.API_URL, (res) => {
+    https
+        .get(process.env.API_URL, (res) => {
         if (res.statusCode === 200) {
             console.log("GET request sent successfully" + new Date());
         } else {
             console.log("Failed to send GET request: " + res.statusCode + " at " + new Date());
         }
-    }).on("error", (err) => {
+    })
+        .on("error", (err) => {
         console.error("Error sending GET request: " + err.message + " at " + new Date());
     });
 });
